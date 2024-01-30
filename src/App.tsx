7041ec79d358f94b699f29fd25f5e728d94bbf13
Murilo <mbalves1@outlook.com>
2024-01-30 13:16:44 -0300

    [feat]: instalando radix, lucide, shadonui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Github } from 'lucide-react'
+import { Button } from "./components/ui/button";
+import { Separator } from './components/ui/separator';
+
 export function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className='min-h-screen'>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">Desenvolvido por murilo</span>
+
+          <Separator orientation='vertical' className='h-6'></Separator>
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2"/>
+            Github</Button>
+        </div>
+      </div>
+    </div>
   )
 }
